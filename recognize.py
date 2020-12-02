aa28e8719e032e60a81d090923f36d6f2343f6af
olivierbenard <olivier.benard@telecomnancy.net>
2020-12-02 14:06:40 +0100

    maj smooth low-pass filter

--- recognize.py
+++ recognize.py
@@ -2,7 +2,29 @@ import cv2
 import numpy as np
 
 def main():
-    print('Hello World!')
+
+    capture = cv2.VideoCapture(0)
+    left, right, top, bottom = 300, 680, 0, 480 # coordinates of the ROI (Region Of Interest)
+
+    while True:
+
+        _, frame = capture.read()
+        frame = cv2.flip(frame, 1) # vertical mirror
+        clone = frame.copy()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        smoothed = cv2.GaussianBlur(gray, (7,7), 0) # kernel of 7x7 pixels, standard deviation equal to 0 for both X and Y directions
+        roi = smotthed[top:bottom, left:right]
+
+        cv2.imshow('Base environment', clone) # non-treated frame
+        cv2.imshow('Treated environment', roi) # pre-treated frame
+
+        keypress = cv2.waitKey(1)
+
+        if keypress == 27: # 'echap'
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
